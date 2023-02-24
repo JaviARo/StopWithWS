b052a9f9381374edbb44ac1807e84d526b925c21
unknown <javieralemanrodriguez@alumno.ieselrincon.es>
2023-02-24 20:41:11 +0000

    several changes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 8080;
 const server = new WebSocket.Server({ port: PORT }, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
 const usersToSend = [];
 const users = [];
-const letter = [];
+let letter = "";
+let letterSet = false;
+let stop = false;
 
 function sendMessage(message) {
-    usersToSend.forEach((user) => {
-        user.ws.send(JSON.stringify(message));
-    });
+  usersToSend.forEach((user) => {
+    user.ws.send(JSON.stringify(message));
+  });
 }
 
 // function sendMessageToRingTheBell(message) {
@@ -25,75 +27,62 @@ function sendMessage(message) {
 //     });
 // }
 
-server.on('connection', (ws, incoming_request) => {
-    const u = { username: incoming_request.url.split("=")[1] };
-    ws.username = u.username
-    const userRef = { ws };
-    usersToSend.push(userRef);
-    users.push(u);
-    console.log("create")
-    console.log(users)
+server.on("connection", (ws, incoming_request) => {
+  const u = { username: incoming_request.url.split("=")[1] };
+  ws.username = u.username;
+  const userRef = { ws };
+  usersToSend.push(userRef);
+  users.push(u);
+  console.log("create");
+  console.log(users);
 
-    sendMessage({
+  sendMessage({
+    users,
+    letter,
+    letterSet,
+  });
+
+  ws.on("message", (message) => {
+    // console.log("message");
+    // console.log(message);
+    try {
+      const data = JSON.parse(message);
+      // Checking if the message is a valid one
+      if (typeof data.type !== "string") {
+        console.error("Invalid message");
+        return;
+      }
+      switch (data.type) {
+        case "letterSend":
+          letter = data.letter;
+          letterSet = true;
+          break;
+        case "stopSend":
+          stop = true;
+          break;
+      }
+
+      sendMessage({
         users,
-        letter
-    });
+        letter,
+        letterSet,
+        stop
+      });
+    } catch (e) {
+      console.error("Error passing message!", e);
+    }
+  });
 
-    ws.on('message', (message) => {
-        console.log(message);
-        try {
-            const data = JSON.parse(message);
-
-            // Checking if the message is a valid one
-            if (typeof data.type !== 'string') {
-                console.error('Invalid message');
-                return;
-            }
-
-            switch (data.type) {
-                case "ask for help":
-                    helps.push({
-                        helper: data.helper,
-                        helped: data.helped,
-                        timestamp: data.timestamp
-                    });
-                    break;
-                case "next please":
-                    for (let i = 0; i < helps.length; i++) {
-                        if (helps[i].helper == data.helper && helps[i].helped == data.helped) {
-                            helps.splice(i, 1);
-                            break;
-                        }
-                    }
-                    break;
-                // case "ringing the bell":
-                //     sendMessageToRingTheBell({
-                //         helper: data.helper,
-                //         helped: data.helped,
-                //         type: "ringing the bell"
-                //     });
-                //     return;
-            }
-
-            sendMessage({
-                users,
-                helps
-            });
-        } catch (e) {
-            console.error('Error passing message!', e)
-        }
-    });
-
-    ws.on('close', (code, reason) => {
-        for (let i = 0; i < users.length; i++) {
-            console.log(users[i]);
-            if (users[i].username === ws.username) {
-                users.splice(i, 1);
-                usersToSend.splice(i, 1);
-                break;
-            }
-        }
-        console.log("final")
-        console.log(users);
-    });
+  ws.on("close", (code, reason) => {
+    for (let i = 0; i < users.length; i++) {
+      console.log(users[i]);
+      if (users[i].username === ws.username) {
+        users.splice(i, 1);
+        usersToSend.splice(i, 1);
+        break;
+      }
+    }
+    console.log("final");
+    console.log(users);
+  });
 });
--- frontend/src/pages/StopPage.jsx
+++ frontend/src/pages/StopPage.jsx
@@ -1,37 +1,148 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function StopPage() {
+  const [users, setUsers] = useState([]);
+  const [letter, setLetter] = useState();
+  const [letterSet, setLetterSet] = useState(false);
+  const [stop, setStop] = useState(false);
+
+  const [nombre, setNombre] = useState();
+  const [animal, setAnimal] = useState();
+  const [lugar, setLugar] = useState();
+  const [comida, setComida] = useState();
+  const [color, setColor] = useState();
+  const [objeto, setObjeto] = useState();
+
+  const { username } = useParams();
+
+  const ws = useRef();
+
+  const sendLetter = (e) => {
+    e.preventDefault();
+    console.log(letter);
+    ws.current.send(
+      JSON.stringify({
+        letter: letter,
+        type: "letterSend",
+      })
+    );
+  };
+
+  const sendStop = (e) => {
+    e.preventDefault();
+    ws.current.send(
+      JSON.stringify({
+        nombre: nombre,
+        animal: animal,
+        lugar: lugar,
+        comida: comida,
+        color: color,
+        objeto: objeto,
+        type: "stopSend",
+      })
+    );
+  };
+
+  useEffect(() => {
+    //Example: ws://localhost:8080/?username=tiburcio
+    const SERVER_URL = `${process.env.REACT_APP_SERVER_URL}/?username=${username}`;
+    ws.current = new WebSocket(SERVER_URL);
+
+    ws.current.onopen = () => {
+      console.log("Connection opened");
+    };
+
+    ws.current.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      setLetter(data.letter);
+      setLetterSet(data.letterSet);
+      setStop(data.stop);
+      console.log(data);
+    };
+
+    return () => {
+      console.log("Cleaning up...");
+      ws.current.close();
+    };
+  }, [username]);
+
   return (
-    <div>
-      <form id="login-page-username-form">
-        <label htmlFor="login-page-username">Introduce una letra</label>
-        <input id="login-page-username" type="char" placeholder="Letra"
-          value={letter}
-          onChange={(e) => setLetter(e.target.value)}
-          required
-        />
-        <button id="login-page-login-button" type="button" onClick={handleSubmit}>Entrar</button>
-      </form>
-      <table>
-        <th>
-          <td>Letra</td>
-          <td>Nombre</td>
-          <td>Animal</td>
-          <td>Lugar</td>
-          <td>Color</td>
-          <td>Comida</td>
-          <td>Objeto</td>
-        </th>
-        <tr>
-          <td>Letra</td>
-          <td><input></input></td>
-          <td><input></input></td>
-          <td><input></input></td>
-          <td><input></input></td>
-          <td><input></input></td>
-          <td><input></input></td>
-        </tr>
-      </table>
-    </div>
-  )
+    <>
+      {stop === true ? (
+        <div>
+          <p>Fin del juego</p>
+        </div>
+      ) : (
+        <div>
+          {letterSet === true ? (
+            <div>
+              <p>Letra: {letter}</p>
+              <form onSubmit={sendStop}>
+                <label htmlFor="letra">Nombre</label>
+                <input
+                  id="nombre"
+                  type="char"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                />
+                <label htmlFor="letra">Animal</label>
+                <input
+                  id="animal"
+                  type="char"
+                  value={animal}
+                  onChange={(e) => setAnimal(e.target.value)}
+                />
+                <label htmlFor="letra">Lugar</label>
+                <input
+                  id="lugar"
+                  type="char"
+                  value={lugar}
+                  onChange={(e) => setLugar(e.target.value)}
+                />
+                <label htmlFor="letra">Color</label>
+                <input
+                  id="color"
+                  type="char"
+                  value={color}
+                  onChange={(e) => setColor(e.target.value)}
+                />
+                <label htmlFor="letra">Comida</label>
+                <input
+                  id="comida"
+                  type="char"
+                  value={comida}
+                  onChange={(e) => setComida(e.target.value)}
+                />
+                <label htmlFor="letra">Objeto</label>
+                <input
+                  id="objeto"
+                  type="char"
+                  value={objeto}
+                  onChange={(e) => setObjeto(e.target.value)}
+                />
+                <button type="submit">Stop</button>
+              </form>
+            </div>
+          ) : (
+            <form onSubmit={sendLetter}>
+              <label htmlFor="login-page-username">Introduce una letra</label>
+              <input
+                id="login-page-username"
+                type="char"
+                placeholder="Letra"
+                value={letter}
+                onChange={(e) => setLetter(e.target.value)}
+                required
+              />
+              <button onClick={sendLetter}>Enviar letra</button>
+            </form>
+          )}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default StopPage;
